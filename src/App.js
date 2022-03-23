2435822bb4954a4d893362ef9adb141291654939
vijay <vijayramreddy990@gmail.com>
2022-03-24 00:41:05 +0530

    signup changes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Nav from "./components/Nav";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Footer from "./components/Footer";
+import SignUp from "./components/SignUp";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+        <Nav />
+        <Routes>
+          <Route path="/" element={<h1>Product Component</h1>} />
+          <Route path="/add" element={<h1>Add Component</h1>} />
+          <Route path="/update" element={<h1>Update Component</h1>} />
+          <Route path="/logout" element={<h1>Logout</h1>} />
+          <Route path="/profile" element={<h1>Profile</h1>} />
+          <Route path="/signup" element={<SignUp />} />
+        </Routes>
+      </BrowserRouter>
+      <Footer />
     </div>
   );
 }
---
+++ src/components/SignUp.js
@@ -0,0 +1,42 @@
+import React, { useState } from "react";
+
+const SignUp = () => {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const collectData = () => {
+    console.warn(name, email, password);
+  };
+  return (
+    <div className="register">
+      <h1>Register</h1>
+      <input
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+        className="inputBox"
+        type={"text"}
+        placeholder="Enter Name"
+      />
+      <input
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+        className="inputBox"
+        type={"text"}
+        placeholder="Enter Email"
+      />
+      <input
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+        className="inputBox"
+        type={"password"}
+        placeholder="Enter Password"
+      />
+      <button onClick={collectData} type="button" className="appButton">
+        Sign Up
+      </button>
+    </div>
+  );
+};
+
+export default SignUp;
