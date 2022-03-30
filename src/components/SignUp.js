adf8958249459f84b1460c8c44440d992d1419d0
vijay <vijayramreddy990@gmail.com>
2022-03-30 16:31:26 +0530

    signup api

--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -1,12 +1,21 @@
 import React, { useState } from "react";
-
+import { useNavigate } from "react-router-dom";
 const SignUp = () => {
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-
-  const collectData = () => {
-    console.log(name, email, password);
+  const navigate = useNavigate();
+  const collectData = async () => {
+    let result = await fetch("http://localhost:5000/register", {
+      method: "post",
+      body: JSON.stringify({ name, email, password }),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    result = await result.json();
+    console.warn(result);
+    navigate("/");
   };
   return (
     <div className="register">
