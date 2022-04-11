78189e0dfb1707542c1b50cfb3e74e2e919506bf
vijay <vijayramreddy990@gmail.com>
2022-04-11 13:25:55 +0530

    logout

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,8 +1,13 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 const Nav = () => {
   const auth = localStorage.getItem("user");
+  const navigate = useNavigate();
+  const logout = () => {
+    localStorage.clear();
+    navigate("/signup");
+  };
   return (
     <div>
       <ul className="nav-ul">
@@ -20,7 +25,9 @@ const Nav = () => {
         </li>
         <li>
           {auth ? (
-            <Link to="/logout">Logout</Link>
+            <Link onClick={logout} to="/signup">
+              Logout
+            </Link>
           ) : (
             <Link to="/signup">Sign Up</Link>
           )}
